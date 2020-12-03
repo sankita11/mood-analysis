1e70ef7e4d564696bcfaab5e1265052da0ae1971
Ankita <sankita.11@gmail.com>
2020-12-03 23:54:08 +0000

    updated config.js

--- mood-analysis-api/config/config.js
+++ mood-analysis-api/config/config.js
@@ -9,6 +9,4 @@ module.exports = {
         connectionLimit: 10,
         queueLimit: 0
     },
-    aws_access_key_id: "AKIAV35PLLFONGIUY37B",
-    AWS_SECRET_ACCESS_KEY: "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"
 }
