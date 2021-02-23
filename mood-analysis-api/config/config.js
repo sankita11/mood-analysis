2bab6bd7aabbe3cb8d4f074a91cde2db0fc12d62
Ankita <sankita.11@gmail.com>
2021-02-23 16:20:05 +0530

    Update config.js

--- mood-analysis-api/config/config.js
+++ mood-analysis-api/config/config.js
@@ -2,9 +2,9 @@ module.exports = {
     port: 3002,
     dbParams: {
         host: "localhost",
-        user: "root",
-        password: "112020DB",
-        database: "unmind",
+        user: "",
+        password: "",
+        database: "",
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0
